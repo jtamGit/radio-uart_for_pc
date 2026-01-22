1da2e7d499a96d3cadb159b32ad2c98591c40272
jtamGit <86784608+jtamGit@users.noreply.github.com>
2026-01-22 16:45:58 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -4,60 +4,29 @@ def on_received_string(receivedString):
 radio.on_received_string(on_received_string)
 
 buttonVal = 0
-item = 0
-def button():
-    global buttonVal, item
-    buttonVal = pins.analog_read_pin(AnalogReadWritePin.P2)
-    if buttonVal < 256:
-        item = 1
-    elif buttonVal < 597:
-        item = 2
-    elif buttonVal < 725:
-        item = 3
-    elif buttonVal < 793:
-        item = 4
-    elif buttonVal < 836:
-        item = 5
-    elif buttonVal < 938:
-        item = 6
-    else:
-        item = 0
-
+y_bar = 0
+x_bar = 0
 res = ""
 radio.set_group(1)
+radio.set_transmit_power(7)
 serial.redirect_to_usb()
 serial.set_tx_buffer_size(1000)
 serial.set_rx_buffer_size(1000)
 serial.set_baud_rate(BaudRate.BAUD_RATE4800)
-images.create_image("""
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . #
-    . # # # .
-    """).show_image(0)
+# serial.write_value("x=", x_bar)
+# serial.write_value("y=", y_bar)
+# serial.write_value("b=", buttonVal)
 
 def on_forever():
-    global res
+    global res, x_bar, y_bar, buttonVal
     res = serial.read_string()
     if res.is_empty():
         pass
     else:
         radio.send_string(res)
         led.toggle(4, 0)
-
-    button()
-    if item:
-        basic.show_number(item)
-    elif pins.analog_read_pin(AnalogReadWritePin.P0) < 400:
-        basic.show_string("-X")
-    elif pins.analog_read_pin(AnalogReadWritePin.P0) > 600:
-        basic.show_string("+X")
-    elif pins.analog_read_pin(AnalogReadWritePin.P1) < 400:
-        basic.show_string("-Y")
-    elif pins.analog_read_pin(AnalogReadWritePin.P1) > 600:
-        basic.show_string("+Y")
-    else:
-        basic.clear_screen()
-
+    x_bar = pins.analog_read_pin(AnalogReadWritePin.P0)
+    y_bar = pins.analog_read_pin(AnalogReadWritePin.P1)
+    buttonVal = pins.analog_read_pin(AnalogReadWritePin.P2)
+    radio.send_value("x", x_bar)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ radio.onReceivedString(function (receivedString) {
     serial.writeString(receivedString)
     led.toggle(0, 0)
 })
+let buttonVal = 0
+let y_bar = 0
+let x_bar = 0
 let res = ""
 radio.setGroup(1)
 radio.setTransmitPower(7)
@@ -9,10 +12,18 @@ serial.redirectToUSB()
 serial.setTxBufferSize(1000)
 serial.setRxBufferSize(1000)
 serial.setBaudRate(BaudRate.BaudRate4800)
+// serial.write_value("x=", x_bar)
+// serial.write_value("y=", y_bar)
+// serial.write_value("b=", buttonVal)
 basic.forever(function () {
     res = serial.readString()
     if (res.isEmpty()) {
-    	
+        x_bar = pins.analogReadPin(AnalogReadWritePin.P0)
+        y_bar = pins.analogReadPin(AnalogReadWritePin.P1)
+        buttonVal = pins.analogReadPin(AnalogReadWritePin.P2)
+        radio.sendValue("x", x_bar)
+        radio.sendValue("y", y_bar)
+        radio.sendValue("button", buttonVal)
     } else {
         radio.sendString(res)
         led.toggle(4, 0)
